0a318f83173ae57e9ac633a0157dde39bb20640d
scrappy1987 <john.gordon@qa.com>
2019-12-10 17:10:28 +0000

    initial commit

---
+++ src/main/java/com/qa/business/TrainerService.java
@@ -0,0 +1,45 @@
+package com.qa.business;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.qa.persistence.domain.Trainer;
+import com.qa.persistence.repository.TrainerRepository;
+
+@Service
+public class TrainerService {
+
+	private TrainerRepository trainerRepo;
+
+	public TrainerService(TrainerRepository trainerRepo) {
+		this.trainerRepo = trainerRepo;
+	}
+
+	public List<Trainer> getAllTrainer() {
+		if (trainerRepo.findAll().isEmpty()) {
+			setUpTrainers();
+		}
+		return trainerRepo.findAll();
+	}
+
+	private void setUpTrainers() {
+		Trainer john = new Trainer("John", "Gordon");
+		Trainer chris = new Trainer("Chris", "Perrins");
+		trainerRepo.save(john);
+		trainerRepo.save(chris);
+	}
+
+	public Trainer addNewTrainer(Trainer trainer) {
+		return trainerRepo.save(trainer);
+	}
+
+	public Trainer updateTrainer(Trainer trainer) {
+		return trainerRepo.save(trainer);
+	}
+
+	public String deleteTrainer(Long id) {
+		trainerRepo.deleteById(id);
+		return "Trainer succesfully deleted";
+	}
+}
